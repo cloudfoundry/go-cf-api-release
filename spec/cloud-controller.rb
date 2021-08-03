8f70c0accbc27344e0a219cb8e1bc1934d39a16b
Andy Paine <andrew.paine@sap.com>
2021-08-03 13:47:11 +0100

    Use DB connection params format from CAPI
    Make the spec for cloudgontroller match the spec for the
    cloud_controller_ng job from CAPI. This means transforming the slightly
    weird properties format from CAPI into connection strings for our 2
    different DB types. It might make sense to eventually make the
    connection string building part of the actual Go code since the BOSH
    release shouldn't really need to know the difference between MySQL and
    Postgres

--- spec/cloud-controller.rb
+++ spec/cloud-controller.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require 'bosh/template/test'
+require 'bosh/template'
 
 describe 'cloud-controller' do
   let(:job_name) { 'cloud-controller' }
@@ -9,28 +10,66 @@ describe 'cloud-controller' do
     let(:template) { job.template('config/bpm.yml') }
 
     it 'uses the psql binary when ccdb.db_scheme is postgres' do
-      process = get_process_from_bpm(YAML::load(template.render({})), job_name)
-      expect(process['executable']).to eq 'cloud-controller-psql'
+      process = get_process_from_bpm(YAML.safe_load(template.render({})), job_name)
+      expect(process['executable']).to eq '/var/vcap/jobs/cloud-controller/packages/cloud-controller/cloud-controller-psql'
     end
 
     it 'uses the mysql binary when ccdb.db_scheme is mysql' do
-      config = {'ccdb' => { 'db_scheme' => 'mysql'}}
-      process = get_process_from_bpm(YAML::load(template.render(config)), job_name)
-      expect(process['executable']).to eq 'cloud-controller-mysql'
+      config = { 'ccdb' => { 'db_scheme' => 'mysql' } }
+      process = get_process_from_bpm(YAML.safe_load(template.render(config)), job_name)
+      expect(process['executable']).to eq '/var/vcap/jobs/cloud-controller/packages/cloud-controller/cloud-controller-mysql'
     end
 
     it 'raises an error if ccdb.db_scheme is not postgres or mysql' do
-      config = {'ccdb' => { 'db_scheme' => 'foo'}}
-      expect{ template.render(config) }.to raise_error('ccdb.db_scheme foo is not supported')
+      config = { 'ccdb' => { 'db_scheme' => 'foo' } }
+      expect { template.render(config) }.to raise_error('ccdb.db_scheme foo is not supported')
     end
 
     it 'passes the config file path to the binary' do
-      process = get_process_from_bpm(YAML::load(template.render({})), job_name)
+      process = get_process_from_bpm(YAML.safe_load(template.render({})), job_name)
       expect(process['args']).to eq ['/var/vcap/jobs/cloud-controller/config/cloud-controller.yml']
+    end
+  end
+
+  describe 'cloud-controller.yml' do
+    let(:template) { job.template('config/cloud-controller.yml') }
+    let(:default_ccdb_config) do
+      {
+        'db_scheme' => 'postgres',
+        'address' => 'db.example.com',
+        'port' => 1234,
+        'databases' => [{
+          'name' => 'ccdb', 'tag' => 'cc'
+        }],
+        'roles' => [{
+          'name' => 'admin_user', 'password' => 'admin_password', 'tag' => 'admin'
+        }]
+      }
+    end
+
+    it 'creates a psql connection string' do
+      config_file = YAML.safe_load(template.render({ 'ccdb' => default_ccdb_config }))
+      expect(config_file['db']['connectionstring']).to eq 'host=db.example.com port=1234 user=admin_user dbname=ccdb password=admin_password sslmode=disable'
+    end
+
+    it 'creates a mysql connection string' do
+      ccdb_config = default_ccdb_config.merge({ 'db_scheme' => 'mysql' })
+      config_file = YAML.safe_load(template.render({ 'ccdb' => ccdb_config }))
+      expect(config_file['db']['connectionstring']).to eq 'admin_user:admin_password@tcp(db.example.com:1234)/ccdb?tls=false&parseTime=true'
+    end
+
+    it 'raises an error when address is not set' do
+      ccdb_config = default_ccdb_config.reject { |k, _| k == 'address' }
+      expect { template.render({ 'ccdb' => ccdb_config }) }.to raise_error(Bosh::Template::UnknownProperty, "Can't find property '[\"ccdb.address\"]'")
+    end
+
+    it 'raises an error when port is not set' do
+      ccdb_config = default_ccdb_config.reject { |k, _| k == 'port' }
+      expect { template.render({ 'ccdb' => ccdb_config }) }.to raise_error(Bosh::Template::UnknownProperty, "Can't find property '[\"ccdb.port\"]'")
     end
   end
 end
 
 def get_process_from_bpm(bpm_config, job_name)
-  return bpm_config['processes'].select { |p| p['name'] == job_name }.first
+  bpm_config['processes'].select { |p| p['name'] == job_name }.first
 end
