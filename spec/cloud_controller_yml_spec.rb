2c098f6645fa244f50599661c917cd2f008c3089
Andy Paine <andrew.paine@sap.com>
2021-08-10 12:25:09 +0100

    Bump source code module and add UAA config
    Currently we cannot provide a ca-certificate but we can work around this
    with the os-conf release I think!

--- spec/cloud_controller_yml_spec.rb
+++ spec/cloud_controller_yml_spec.rb
@@ -28,30 +28,51 @@ describe 'cloud-controller' do
       }
     )
   end
-  subject(:rendering) { YAML.safe_load(template.render({}, consumes: [ccdb_link]))['db'] }
 
-  context 'db_scheme is postgres' do
-    it { is_expected.to include('connectionstring' => 'host=db.example.com port=1234 user=admin_user dbname=ccdb password=admin_password sslmode=disable') }
-  end
+  describe 'database configuration' do
+    subject(:rendering) { YAML.safe_load(template.render({}, consumes: [ccdb_link]))['db'] }
+    context 'db_scheme is postgres' do
+      it { is_expected.to include('connectionstring' => 'host=db.example.com port=1234 user=admin_user dbname=ccdb password=admin_password sslmode=disable') }
+    end
 
-  context 'db_scheme is mysql' do
-    let(:ccdb_config) { super().merge({ 'db_scheme' => 'mysql' }) }
-    it { is_expected.to include('connectionstring' => 'admin_user:admin_password@tcp(db.example.com:1234)/ccdb?tls=false&parseTime=true') }
-  end
+    context 'db_scheme is mysql' do
+      let(:ccdb_config) { super().merge({ 'db_scheme' => 'mysql' }) }
+      it { is_expected.to include('connectionstring' => 'admin_user:admin_password@tcp(db.example.com:1234)/ccdb?tls=false&parseTime=true') }
+    end
 
-  context 'missing address property' do
-    let(:ccdb_config) { super().reject { |k, _| k == 'address' } }
+    context 'missing address property' do
+      let(:ccdb_config) { super().reject { |k, _| k == 'address' } }
 
-    it 'raises an error' do
-      expect { rendering }.to raise_error(KeyError, 'key not found: "address"')
+      it 'raises an error' do
+        expect { rendering }.to raise_error(KeyError, 'key not found: "address"')
+      end
+    end
+
+    context 'missing port property' do
+      let(:ccdb_config) { super().reject { |k, _| k == 'port' } }
+
+      it 'raises an error' do
+        expect { rendering }.to raise_error(KeyError, 'key not found: "port"')
+      end
     end
   end
 
-  context 'missing port property' do
-    let(:ccdb_config) { super().reject { |k, _| k == 'port' } }
+  describe 'uaa configuration' do
+    let(:uaa_config) { {} }
+    subject(:rendering) { YAML.safe_load(template.render({ 'uaa' => uaa_config }, consumes: [ccdb_link]))['uaa'] }
 
-    it 'raises an error' do
-      expect { rendering }.to raise_error(KeyError, 'key not found: "port"')
+    context 'defaults are used' do
+      it { is_expected.to include('url' => 'https://uaa.service.cf.internal:8443') }
+    end
+
+    context 'url and port are provided' do
+      let(:uaa_config) do
+        {
+          'internal_url' => 'custom.uaa.hostname',
+          'tls_port' => 8888
+        }
+      end
+      it { is_expected.to include('url' => 'https://custom.uaa.hostname:8888') }
     end
   end
 end
