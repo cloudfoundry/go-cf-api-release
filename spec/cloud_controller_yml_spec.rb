63cd8f6bee5022e32d28bc5b172fa949bcf59535
Andy Paine <andrew.paine@sap.com>
2021-10-13 11:06:37 +0100

    Add properties, templates and test for urls/app_ssh
    These properties are needed for the '/' endpoint

--- spec/cloud_controller_yml_spec.rb
+++ spec/cloud_controller_yml_spec.rb
@@ -36,7 +36,7 @@ describe 'cloud-controller' do
         'cc' => {
           'external_host' => 'api',
           'external_protocol' => 'https'
-        },
+        }
       }
     )
   end
@@ -97,7 +97,6 @@ describe 'cloud-controller' do
     end
   end
 
-
   describe 'cc info configuration' do
     let(:cc_config) { {} }
     subject(:rendering) { YAML.safe_load(template.render(cc_config, consumes: [ccdb_link, ccinternal_link]))['info'] }
@@ -113,12 +112,12 @@ describe 'cloud-controller' do
     context 'info is provided' do
       let(:cc_config) do
         super().merge({
-          'name' => 'test',
-          'build'  => 'custom',
-          'support_address' => 'help@example.com',
-          'version' => 1,
-          'description' => 'test',
-        })
+                        'name' => 'test',
+                        'build' => 'custom',
+                        'support_address' => 'help@example.com',
+                        'version' => 1,
+                        'description' => 'test'
+                      })
       end
       it { is_expected.to include('name' => 'test') }
       it { is_expected.to include('build' => 'custom') }
@@ -128,8 +127,73 @@ describe 'cloud-controller' do
     end
   end
 
+  describe 'cc urls configuration' do
+    subject(:rendering) { YAML.safe_load(template.render(cc_config, consumes: [ccdb_link, ccinternal_link]))['urls'] }
+
+    context 'uses defaults based on system_domain from link' do
+      let(:cc_config) { {} }
+      it { is_expected.to include('log_cache' => 'https://log-cache.example.com') }
+      it { is_expected.to include('log_stream' => 'https://log-stream.example.com') }
+      it { is_expected.to include('doppler' => 'wss://doppler.example.com:443') }
+      it { is_expected.to include('login' => 'https://login.example.com') }
+      it { is_expected.to include('uaa' => 'https://uaa.example.com') }
+    end
+
+    context 'doppler config is provided' do
+      let(:cc_config) { { 'doppler' => { 'use_ssl' => false, 'port' => 4443 } } }
+      it { is_expected.to include('doppler' => 'ws://doppler.example.com:4443') }
+    end
+
+    context 'login is disabled' do
+      let(:cc_config) { { 'login' => { 'enabled' => false } } }
+      it { is_expected.to include('login' => 'https://uaa.example.com') }
+    end
+
+    context 'login protocol is provided' do
+      let(:cc_config) { { 'login' => { 'protocol' => 'http' } } }
+      it { is_expected.to include('login' => 'http://login.example.com') }
+    end
+
+    context 'login url is provided' do
+      let(:cc_config) { { 'login' => { 'url' => 'https://custom.login.url' } } }
+      it { is_expected.to include('login' => 'https://custom.login.url') }
+    end
+
+    context 'uaa url is provided' do
+      let(:cc_config) { { 'login' => { 'enabled' => false }, 'uaa' => { 'url' => 'https://custom.uaa.url' } } }
+      it { is_expected.to include('login' => 'https://custom.uaa.url') }
+      it { is_expected.to include('uaa' => 'https://custom.uaa.url') }
+    end
+  end
+
+  describe 'cc app_ssh configuration' do
+    subject(:rendering) { YAML.safe_load(template.render(cc_config, consumes: [ccdb_link, ccinternal_link]))['app_ssh'] }
+
+    context 'uses defaults based on system_domain from link' do
+      let(:cc_config) { {} }
+      it { is_expected.to include('endpoint' => 'ssh.example.com:2222') }
+      it { is_expected.to include('oauth_client' => 'ssh-proxy') }
+      it { is_expected.to include('host_key_fingerprint' => nil) }
+    end
+
+    context 'port is provided' do
+      let(:cc_config) { { 'app_ssh' => { 'port' => 22 } } }
+      it { is_expected.to include('endpoint' => 'ssh.example.com:22') }
+    end
+
+    context 'oauth_client_id is provided' do
+      let(:cc_config) { { 'app_ssh' => { 'oauth_client_id' => 'custom-client-id' } } }
+      it { is_expected.to include('oauth_client' => 'custom-client-id') }
+    end
+
+    context 'host_key_fingerprint is provided' do
+      let(:cc_config) { { 'app_ssh' => { 'host_key_fingerprint' => 'b8:80:2c:8c:d7:25:ad:2a:b4:8c:02:34:52:06:f7:ba:1f:0d:02:de' } } }
+      it { is_expected.to include('host_key_fingerprint' => 'b8:80:2c:8c:d7:25:ad:2a:b4:8c:02:34:52:06:f7:ba:1f:0d:02:de') }
+    end
+  end
+
   describe 'cc configuration' do
-    subject(:rendering) { YAML.safe_load(template.render( {}, consumes: [ccdb_link, ccinternal_link])) }
+    subject(:rendering) { YAML.safe_load(template.render({}, consumes: [ccdb_link, ccinternal_link])) }
 
     context 'link provides properties' do
       it { is_expected.to include('external_domain' => 'api.example.com') }
